b702027b313c42be5c4cf88b8e7fe1265a496aac
coderowson <runurowson@gmail.com>
2022-03-25 22:56:15 +0600

    start

---
+++ src/Components/GoodFoods/GoodFoods.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+
+const GoodFoods = () => {
+    const [foods, setFoods] = useState([])
+
+    useEffect(() => {
+
+    }, [])
+
+    return (
+        <div>
+            <h3>Eat to live or Love to eat</h3>
+        </div>
+    );
+};
+
+export default GoodFoods;
